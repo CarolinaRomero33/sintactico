1fc9c9e1a5c87094ede4bffa6b84d4b9bb062788
Andrea <andrea.romero16@tectijuana.edu.mx>
2016-12-09 01:20:54 -0800

    Add files via upload

---
+++ token.go
@@ -0,0 +1,56 @@
+package andrea
+
+// Token represents a lexical token.
+type Token int
+
+const (
+	
+	// Special tokens
+	ILLEGAL Token = iota
+	EOF
+	WS
+	// Literals
+	IDENT // main
+	// Misc characters
+	ASTERISK      // *
+	COMMA         // ,
+	DosPunt       // :
+	ParDer        // (
+	ParIsq        // )
+	PuntCom       // ;
+	LlaveDer      // {
+	LlaveIsq      // }
+	SaltoDeL      //n
+	Asignaicon    // =
+	corchetederecho // [
+	corcheteizquierdo // ]
+
+	// Keywords
+	SELECT
+	FROM
+	si
+	sw
+	zend
+	zINT // Tipodedato
+	funcn
+	zmain
+	no
+	para
+	zcase
+	vars // variables
+	breaks
+	while
+	tri
+	du
+	zUNCHECKED
+	
+	tri
+	cach
+	zFINALLY
+	zsize
+	znew
+	zout
+	feach
+	clase
+	zthis
+)
